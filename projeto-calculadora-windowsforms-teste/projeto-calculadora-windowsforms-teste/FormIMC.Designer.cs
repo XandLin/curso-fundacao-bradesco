387ddba93142d22b9a798b1ae889dc43cbc1f35c
Xand Lin <allex.santos135@gmail.com>
2021-02-23 17:53:23 -0300

    atualizando

---
+++ projeto-calculadora-windowsforms-teste/projeto-calculadora-windowsforms-teste/FormIMC.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+namespace projeto_calculadora_windowsforms_teste
+{
+    partial class FormIMC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormIMC
+            // 
+            this.AccessibleDescription = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "FormIMC";
+            this.Text = "Calcular IMC (Índice de Massa Corporal)";
+            this.Load += new System.EventHandler(this.Fom2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
